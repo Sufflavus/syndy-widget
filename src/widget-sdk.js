9b0e6590a213524b5593beb650bd0c4ff8f111a7
Tania Shatilova <tatashat@gmail.com>
2016-09-20 00:20:03 +0400

    + example of usage

--- src/widget-sdk.js
+++ src/widget-sdk.js
@@ -3,13 +3,13 @@
 (function() {
     var nutrientsClassName = "syndy-nutrients";
     var requiredAttributeName = "product-id";
-    var contentUri = "widget.html";
+    var contentUri = "/src/widget.html";
     var widgetWidth = "500px";
     var widgetHeight = "700px";
 
     var nutrients = document.getElementsByClassName(nutrientsClassName);
 
-    for(var i = 0, length = nutrients.length; i++) {
+    for(var i = 0, length = nutrients.length; i < length; i++) {
         var element = nutrients[i];
         if(!element.hasAttribute(requiredAttributeName)) {
             continue;
@@ -24,6 +24,9 @@
         widgetFrame.setAttribute("src", contentUri);
         widgetFrame.style.width = widgetWidth;
         widgetFrame.style.height = widgetHeight;
+        widgetFrame.style.border = 0;
+        widgetFrame.style.frameborder = 0;
+        widgetFrame.style.overflow = "hidden";
         return widgetFrame;
     }
 })();
